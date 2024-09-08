ba6364ac0ddd68bd9ed772907f0f2f995a8f9377
ziddigsm <158247382+ziddigsm@users.noreply.github.com>
2024-09-08 11:34:43 -0400

    added Education tab

--- src/content/Home/Home.tsx
+++ src/content/Home/Home.tsx
@@ -11,7 +11,7 @@ function Home() {
   if (themeProps.theme === true) 
     theme = "dark";
     const bio = "Embarking on a journey through the digital cosmos, I’m Ghouse Shahe Meera Ziddi Mohammad, a navigator of the data science realm, currently honing my skills in Computer Engineering at the University of Cincinnati. As I continue to master my craft, I’m on the lookout for challenging internships to apply my knowledge and further sharpen my abilities."
-    const exp = "Ex-Accenture - India | Ex-Efftronics - India"
+    const exp = "Ex-Accenture | Ex-Efftronics"
     return (
         <div className="home" id="Home" data-theme = {theme}>
         <div className="name" >
