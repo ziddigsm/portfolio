2a8254a2d81b9d1021b2da415f60a6e92f1b519f
ziddigsm <158247382+ziddigsm@users.noreply.github.com>
2024-11-14 01:34:35 -0500

    added projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,25 +4,41 @@ import { FaGithub } from "react-icons/fa";
 function Projects() {
   const projects = [
     {
+      title: "ThoughtHub - Blogging Platform (Frontend)",
+      description:
+        "ThoughtHub is a full-stack web application that allows users to create, read, update, and delete blogs in various categories such as Tech, Science, Cooking, Entertainment, and more. Built with a modern tech stack, it uses React for the frontend, GoLang for the backend, and PostgreSQL for the database. The app also includes Google OAuth authentication and integrated Generative AI for blog summarization.",
+      githubLink: "https://github.com/ziddigsm/thoughtHub_Frontend",
+    },
+    {
+      title: "ThoughtHub - Blogging Platform (Backend)",
+      description:
+        "ThoughtHub is a full-stack web application enabling blog CRUD operations across multiple categories. Built with React (frontend), GoLang (backend), and PostgreSQL (database), it features Google OAuth for user authentication and Generative AI for blog summarization.",
+      githubLink: "https://github.com/ziddigsm/thoughtHub_Backend",
+    },
+    {
       title: "Bard Tales",
-      description: "Bard Tales is a web application designed to take in a story in text format, and then recite it back with emotionally relevant musical background. The project uses AWS Polly for text-to-speech conversion, Meta's MusicGen AI model to generate music, Google Gemini for prompt engineering, and Flask for the backend, in addition to using HTML and CSS for the frontend. ",
+      description:
+        "Bard Tales is a web app that takes a text-based story and recites it with emotionally relevant musical backgrounds. This project uses AWS Polly for TTS, Meta's MusicGen AI for music generation, Google Gemini for prompt engineering, Flask for the backend, and HTML/CSS for the frontend.",
       githubLink: "https://github.com/ziddigsm/BardTales",
     },
     {
       title: "Portfolio",
-      description: "React Portfolio is a personal website, built using React and TypeScript, that includes key core concepts of React like hooks, reusable JSX components, and responsive design along with React-Bootstrap. It has a form for user to input their information and reach out via email, and is publicly accessible, deployed on GitHub Pages. ",
+      description:
+        "React Portfolio is a personal website using React and TypeScript, showcasing React hooks, reusable JSX components, and responsive design with React-Bootstrap. It includes a contact form and is deployed on GitHub Pages.",
       githubLink: "https://github.com/ziddigsm/portfolio",
     },
     {
       title: "Flowlog Parser",
-      description: "The application Flowlog Parser is a backend server with single API developed with Node.js and Express.js that takes in flowlog files and correlates them with a lookup table (CSV format) based on destination port and protocol. The output of this application is a CSV file that contains the associated results and an additional table for identifying all port-protocol combinations & their counts. ",
+      description:
+        "Flowlog Parser is a Node.js and Express.js API server that processes flowlog files, correlating them with a CSV lookup table based on port and protocol. It outputs a CSV file containing results and a table of port-protocol counts.",
       githubLink: "https://github.com/ziddigsm/FlowLogParsing",
     },
     {
-        title: "Underwater Target Tracking",
-        description: "This project tracks underwater targets using passive sonar data, employing a combination of Particle Filtering (PF) and Extended Kalman Filter (EKF) to enhance accuracy in nonlinear environments. It aims to improve target position, speed, and course estimates while addressing tracking errors.",
-        githubLink: "",
-      },
+      title: "Underwater Target Tracking",
+      description:
+        "This project utilizes passive sonar data to track underwater targets, combining Particle Filtering (PF) and Extended Kalman Filter (EKF) to improve target position, speed, and course estimates in nonlinear environments.",
+      githubLink: "",
+    },
   ];
 
   return (
@@ -33,7 +49,16 @@ function Projects() {
           <div className="project" key={index}>
             <h3 className="project-title">{project.title}</h3>
             <p className="project-description">{project.description}</p>
-            {!project.githubLink ? project.githubLink : <a href={project.githubLink} target='_blank' className='icon github'><FaGithub className='social github'/></a>}
+            {project.githubLink && (
+              <a
+                href={project.githubLink}
+                target="_blank"
+                className="icon github"
+                rel="noopener noreferrer"
+              >
+                <FaGithub className="social github" />
+              </a>
+            )}
           </div>
         ))}
       </div>
